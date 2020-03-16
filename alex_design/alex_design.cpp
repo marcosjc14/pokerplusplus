8fbfba2641479586e83634b9f45508ce8d76d63f
Alex Pham <alex.pham2@mavs.uta.edu>
2020-03-16 18:10:46 -0500

    these are my files for the design part due on 3/18

---
+++ alex_design/alex_design.cpp
@@ -0,0 +1,25 @@
+#include "alex_design.hpp"
+
+//----------------all methods for dealer class------------------
+dealer::dealer()
+{
+}
+dealer::~dealer()
+{
+}
+
+//----------------all methods for pot class---------------------
+pot::pot()
+{
+}
+pot::~pot()
+{
+}
+
+//----------------all methods for round class-------------------
+round::round()
+{
+}
+round::~round()
+{
+}
---
+++ alex_design/alex_design.hpp
@@ -0,0 +1,33 @@
+#ifndef ALEX_DESIGN_HPP
+#define ALEX_DESIGN_HPP
+
+#include <cstdio>
+#include <cstdlib>
+
+class dealer
+{
+  public:
+
+    ~dealer();
+  private:
+
+};
+
+class pot
+{
+  public:
+
+    ~pot();
+  private:
+};
+
+class round
+{
+  public:
+    
+    ~round();
+  private:
+
+};
+
+#endif
