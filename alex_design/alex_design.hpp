9d10c90a9110aa5d35bdf7a2b01a981701bd5390
Alex Pham <alex.pham2@mavs.uta.edu>
2020-03-16 22:28:09 -0500

    these are my (Alex) files for the design due on 3/18

--- alex_design/alex_design.hpp
+++ alex_design/alex_design.hpp
@@ -7,24 +7,40 @@
 class dealer
 {
   public:
-
+    dealer();
     ~dealer();
-  private:
+    void dealCards(int cardValues[], int aceValues[]);
+    void dealChips(int chipValues[]);
+    int gameResult(int win, int lose);
+    void check();
+    void bet();
+    void raise();
+    void call();
+    void fold();
 
+  private:
+    int cardValues[12];
+    int aceValues[2];
+    int chipValues[3];
+    int win;
+    int lose;
 };
 
 class pot
 {
   public:
-
+    pot();
     ~pot();
+    int addToPot(int potValue);
+
   private:
+    int potValue;
 };
 
 class round
 {
   public:
-    
+    round();
     ~round();
   private:
 
